68c7d6b9fa734c34532764e9626a1c3cd69df20e
hiroshi <gonzalesromanhiroshiyaisen@gmail.com>
2022-11-28 11:18:24 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "PokedexApp",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
