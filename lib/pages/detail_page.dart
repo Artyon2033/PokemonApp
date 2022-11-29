981416330ec4fb38ea3bd36fe3a0a141305769e3
hiroshi <gonzalesromanhiroshiyaisen@gmail.com>
2022-11-29 17:59:40 -0500

    noveno commit

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/ui/widgets/item_data_widget.dart';
 import 'package:pokedex/ui/widgets/item_type_widget.dart';
 
 class DetailPage extends StatelessWidget {
@@ -6,19 +7,29 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: Color(0xff49D0B0),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Color(0xff49D0B0),
-        actions: [IconButton(onPressed: () {}, icon: Icon(
-          Icons.favorite_border_outlined
-        )
-        )
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: Icon(Icons.favorite_border_outlined))
         ],
       ),
       body: Stack(
         children: [
+          Positioned(
+            top: height * 0.1,
+            right: -30,
+            child: Image.asset(
+              'assets/images/pokeball.png',
+              height: 200,
+              color: Colors.white.withOpacity(0.2),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Row(
@@ -29,60 +40,98 @@ class DetailPage extends StatelessWidget {
                   children: [
                     Text(
                       "Bulbasaur",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     const SizedBox(
                       height: 6.0,
-                      ),
-                  Row(
-                    children: [
-                      ItemTypeWidget(text: "Grass",),
-                      ItemTypeWidget(text: "Poison",),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      children: [
+                        ItemTypeWidget(
+                          text: "Grass",
+                        ),
+                        ItemTypeWidget(
+                          text: "Poison",
+                        ),
+                      ],
+                    ),
                   ],
-                )
-                ,
-                Text("#001",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
                 ),
+                Text(
+                  "#001",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 )
               ],
             ),
           ),
           Column(
             children: [
-                const Expanded(
-                flex: 4,
-                child: SizedBox()
-              ),
+              const Expanded(flex: 4, child: SizedBox()),
               Expanded(
-                flex: 8,
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30.0),
-                      topRight: Radius.circular(30.0),
-                    )
-                  ),
-                  child: Stack(
-                    children: [
-                      //Data del Pokemon
-                      Text("About Pokemon"),
-                      //Imagen del Pokemon
-                    ],
-                  ),
-                )
-              ),
+                  flex: 8,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          topRight: Radius.circular(30.0),
+                        )),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        //Data del Pokemon
+                        Padding(
+                          padding: const EdgeInsets.all(22.0),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "About Pokemon",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              ItemDataWidget(
+                                title: "Height",
+                                data: "1.5 m",
+                              ),
+                              ItemDataWidget(
+                                title: "Weight",
+                                data: "6.9 kg",
+                              ),
+                              ItemDataWidget(
+                                title: "Candy",
+                                data: "Bulbasaur Candy",
+                              ),
+                              ItemDataWidget(
+                                title: "Candy Count",
+                                data: "25",
+                              ),
+                            ],
+                          ),
+                        )
+                        //Imagen del Pokemon
+                        ,
+                        Positioned.fill(
+                          top: -90.0,
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: Image.network(
+                                "http://www.serebii.net/pokemongo/pokemon/001.png"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
             ],
           )
         ],
